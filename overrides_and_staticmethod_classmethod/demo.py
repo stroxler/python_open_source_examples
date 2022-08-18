32fd48d51b0b95f4aeda10edf2934af3d620bdb5
Steven Troxler <steven.troxler@gmail.com>
2022-08-18 11:25:42 -0700

    Extend demo.py to account for property as well

--- overrides_and_staticmethod_classmethod/demo.py
+++ overrides_and_staticmethod_classmethod/demo.py
@@ -8,6 +8,10 @@ class Base:
     def normal_method(self) -> None:
         pass
 
+    @property
+    def prop(self) -> int:
+        return 42
+
     @classmethod
     def class_method(cls) -> None:
         pass
@@ -16,27 +20,19 @@ class Base:
     def static_method() -> None:
         pass
 
-
-class Above(Base):
-    @override
-    def normal_method(self) -> None:
-        pass
-
-    @override
-    @classmethod
-    def class_method(cls) -> None:
-        pass
-
-    @override
-    @staticmethod
-    def static_method() -> None:
-        pass
 
 
 class Below(Base):
+
     @override
     def normal_method(self) -> None:
         pass
+
+    @property
+    @override
+    def prop(self) -> int:
+        return 42
+
 
     @classmethod
     @override
@@ -50,12 +46,51 @@ class Below(Base):
 
 
 print(f"Below.normal_method: {getattr(Below.normal_method, '__override__')}")
+print(f"Below.prop: {getattr(Below.static_method, '__override__')}")
 print(f"Below().normal_method: {getattr(Below().normal_method, '__override__')}")
 print(f"Below.class_method: {getattr(Below.class_method, '__override__')}")
 print(f"Below.static_method: {getattr(Below.static_method, '__override__')}")
 
+
+prop_message = None
+
+class Above(Base):
+    @override
+    def normal_method(self) -> None:
+        pass
+
+    try:
+        @override
+        @property
+        def prop(self) -> int:
+            return 42
+    except AttributeError:
+        global prop_message
+        prop_message = "Above.prop: Could not even set __override__ on property descriptor!"
+
+    @override
+    @classmethod
+    def class_method(cls) -> None:
+        pass
+
+    @override
+    @staticmethod
+    def static_method() -> None:
+        pass
+
+
+
+
 print(f"Above.normal_method: {getattr(Above.normal_method, '__override__')}")
 print(f"Above().normal_method: {getattr(Above().normal_method, '__override__')}")
+if prop_message is not None:
+    print(prop_message)
+else:
+    # this code doesn't wind up being run
+    try:
+        print(f"Above.prop: {getattr(Above.prop, '__override__')}")
+    except AttributeError:
+        print("Above.prop has no __override__ attribute")
 try:
     print(f"Above.class_method: {getattr(Above.class_method, '__override__')}")
 except AttributeError:
